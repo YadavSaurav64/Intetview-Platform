1657fe6ac7e46a5a3464a48a2288f81432c39730
YadavSaurav64 <yadavsaurav2801@gmail.com>
2026-07-06 16:09:45 +0530

    validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,11 @@ import {ENV} from "./env.js"
 
 export const connectDB = async()=>{
   try {
+
+    if(!ENV.DB_URL){
+      throw new Error("Missing Database URL")
+    }
+
     const conn = await mongoose.connect(ENV.DB_URL)
     console.log("connected to database:",conn.connection.host)
   } catch (error) {
