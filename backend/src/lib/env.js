c29a0dff3f22b277c9ba5832e956c5985d4833b1
YadavSaurav64 <yadavsaurav2801@gmail.com>
2026-07-07 15:40:42 +0530

    auth-db-sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -6,4 +6,11 @@ export const ENV = {
   PORT:process.env.PORT,
   DB_URL:process.env.DB_URL,
   NODE_ENV:process.env.NODE_ENV,
+  INNGEST_SIGNING_KEY:process.env.INNGEST_SIGNING_KEY,
+  INNGEST_EVENT_KEY:process.env.INNGEST_EVENT_KEY,
+  STEAM_API_KEY:process.env.STEAM_API_KEY,
+  STEAM_SECRET_KEY:process.env.STEAM_SECRET_KEY,
+  CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+  CLIENT_URL :process.env.CLIENT_URL
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,20 @@ import express from 'express'
 import path from 'path'
 import { ENV } from './lib/env.js'
 import { connectDB } from './lib/db.js'
+import cors from "cors"
+import {serve} from "inngest/express"
 
 const app=express()
 
 const __dirname=path.resolve()
+
+
+//middleware
+app.use(express.json())
+//creadentials:true allows cookies to be sent with requests from the client
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.get("/hello",(req,res)=>{
   res.status(200).json({msg:"Success"})
