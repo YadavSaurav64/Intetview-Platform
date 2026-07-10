ecc1abe7b23cb240160949792aae0d841a7817d1
YadavSaurav64 <yadavsaurav2801@gmail.com>
2026-07-10 22:46:46 +0530

    minor chnages in variable names

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,12 +7,12 @@ export const protectRoute = [
     try {
       const clerckId = req.auth().userId;
 
-      if(!clerckId) return res.status(401).json({msg:"Unauthorized - Invalid Token"})
+      if(!clerckId) return res.status(401).json({message:"Unauthorized - Invalid Token"})
 
       //find user in db by clerk ID
       const user =await User.findOne({clerckId})
 
-      if(!user) return res.status(401).json({msg:"Unauthorized - User not found"})
+      if(!user) return res.status(401).json({message:"Unauthorized - User not found"})
 
       //attach user to req
       req.user=user
@@ -20,7 +20,7 @@ export const protectRoute = [
       next()
     } catch (error) {
       console.error("Error in protectRoute middleware:",error)
-      res.status(500).json({msg:"Internal Server Error"})
+      res.status(500).json({message:"Internal Server Error"})
     }
   }
 ]
